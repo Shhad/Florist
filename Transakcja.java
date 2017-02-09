28e5384d38d13e7c847593b126f6838c6d582bdd
Shhad <baszak.filip@gmail.com>
2017-02-09 19:26:33 +0100

    Add files via upload
    Simply florist app

---
+++ Transakcja.java
@@ -0,0 +1,39 @@
+package prog_lab2;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+public class Transakcja {
+	Date data; //Nasz klucz transkacji
+	ArrayList<Kwiatek> listaKwiatkow;
+	String imieNazwisko;
+	String adres;
+	double cena;
+	boolean czyZrealizowane;
+	
+	public Transakcja() {
+		cena=0;
+		listaKwiatkow= new ArrayList<Kwiatek>();
+		czyZrealizowane=false;
+		data=new Date();//zapisuje czas w ktorym zostal wywolany w milisek
+	}
+	void setData() {
+		data= new Date();
+	}
+	void setImieNazwisko(String in) {
+		imieNazwisko=in;
+	}
+	void setAdres(String ad) {
+		adres=ad;
+	}
+	void wysweitl() {
+		System.out.println("Klient: "+imieNazwisko);
+		System.out.println("Adres klienta: "+adres);
+		System.out.println("Data zlozenia zamowienia: "+data);
+		if(czyZrealizowane) {
+			System.out.println("Zamowienie potwierdzone.");
+		}
+		else System.out.println("Zamowienie nie potwierdzone.");
+	}
+	
+}

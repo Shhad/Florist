dcf1d1b7ee4d6c652887ecac465e129c4acaf365
Shhad <baszak.filip@gmail.com>
2017-05-08 12:32:16 +0200

    Add files via upload

---
+++ Kwiatek.java
@@ -0,0 +1,32 @@
+
+//Klasa definiujaca obiekt
+//zmienna sa ogolnie dostepne w celu ulatwienia pracy w innych 
+//strukturach programu;  
+public class Kwiatek extends Object {
+	String nazwa;
+	int IloscNaStanie;
+	double cena;
+
+	void setNazwa(String naz) {
+		nazwa=naz;
+	}
+	void setIlosc(int il) {
+		IloscNaStanie=il;
+	}
+	void setCena(double cen) {
+		cena=cen;
+	}
+	String getNazwa() {
+		return nazwa;
+	}
+	int getIlosc() {
+		return IloscNaStanie;
+	} 
+	double getCena() {
+		return cena;
+	}
+	void wyswietl() {
+		System.out.println("Nazwa: "+nazwa);
+		System.out.println("Ilosc obecna: "+IloscNaStanie);
+	}
+}
